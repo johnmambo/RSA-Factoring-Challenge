4409aafce4bae8a3390341a41eeac6dc1d0e3422
john mambo <johnmambo794@gmail.com>
2022-06-26 21:29:20 +0300

    RSA

--- helpers.py
+++ helpers.py
@@ -1,41 +1,21 @@
-def product(lst):
-    """ Return product of all numbers in a list
-    """
-    prod = 1
-    for _ in lst:
-        prod *= _
-    return prod
+#/usr/bin/env python3
+import sys
 
-def gcd(a, b):
-    """ Returns the greatest common divisor of the two numbers
-    """
-    while b:
-        a, b = b, a % b
-    return a
 
-def kth_iroot(n, k):
-    """ Return the integer k-th root of a number by Newton's method
-    """
-    u = n
-    s = n + 1
-    while u < s:
-        s = u
-        t = (k - 1) * s + n // pow(s, k - 1)
-        u = t // k
-    return s
+def primef(n):
+    if n <= 3:
+        return int(n)
+    if n % 2 == 0:
+        return 2
+    elif n % 3 == 0:
+        return 3
+    else:
+        for i in range(5, int((n)**0.5) + 1, 6):
+            if n % i == 0:
+                return int(i)
+            if n % (i + 2) == 0:
+                return primef(n/(i+2))
+    return int(n)
 
-def isqrt(n):
-    """ Return the square root of a number rounded down to the closest integer
-    """
-    if n < 0:
-        raise ValueError("Square root of negative number!")
-    x = int(n)
-    if n == 0:
-        return 0
-    a, b = divmod(x.bit_length(), 2)
-    n = 2 ** (a + b)
-    while True:
-        y = (n + x // n) // 2
-        if y >= x:
-            return x
-        x = y
+
+print(primef(int(sys.argv[1])))
